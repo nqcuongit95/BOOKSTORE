031d8082605754365be5aa21ca9df313eecc9514
Nguyen Quoc Cuong <nqcuongit95@gmail.com>
2016-11-29 20:28:21 +0700

    them customer controller

---
+++ src/BookStore/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookStore.Services;
+using BookStore.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private IBookStoreData _bookStoreData;
+
+        public CustomerController(IBookStoreData bookStoreData)
+        {
+            _bookStoreData = bookStoreData;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _bookStoreData.GetAllCustomer();
+
+            return View(model);
+        }
+
+        public IActionResult CreateCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateCustomer(Khachhang model)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookStoreData.CreateCustomer(model);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+    }
+}
