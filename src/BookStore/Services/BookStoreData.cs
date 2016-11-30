09e27fe67248202b21e381d0a25affba4bb98a9a
trananh2001 <akatsuki200195@gmail.com>
2016-11-30 16:26:50 +0700

    add Don hang

--- src/BookStore/Services/BookStoreData.cs
+++ src/BookStore/Services/BookStoreData.cs
@@ -1,4 +1,5 @@
 ﻿using BookStore.Models;
+using BookStore.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,32 @@ namespace BookStore.Services
         public IEnumerable<KhachHang> GetAllCustomer()
         {
             return _context.KhachHang;
+        }
+
+        public List<object> GetAllDonHang()
+        {
+            //return _context.DonHang;
+
+            var list = (from dh in _context.DonHang
+                        join kh in _context.KhachHang
+                        on dh.KhachHangId equals kh.Id
+                        join trangthai in _context.TrangThai
+                        on dh.TrangThaiId equals trangthai.Id
+                        select new DonHangViewModels
+                        {
+                            NgayLap = dh.NgayLap,
+                            KhachHangID = kh.Id,
+                            KhachHang = kh.TenKhachHang,
+                            TrangThaiID = trangthai.Id,
+                            TrangThai = trangthai.TenTrangThai,
+                            TongTien = dh.TongTien
+                        }).ToList();
+            return new List<object>(list);
+        }
+
+        public IEnumerable<DonHang> GetDonHang()
+        {
+            return _context.DonHang;
         }
     }
 }
