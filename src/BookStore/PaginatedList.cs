4302341dc7266f162270882fc1ff96a1a042050a
DuongHuynhDuy95 <DuongHuynhDuy951@gmail.com>
2016-12-02 13:04:11 +0700

    Fix PaginatedList

--- src/BookStore/PaginatedList.cs
+++ src/BookStore/PaginatedList.cs
@@ -47,10 +47,13 @@ namespace BookStore
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; }
 
-        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        private PaginatedList(List<T> items, int count, int pageIndex, int totalPages)
         {
             PageIndex = pageIndex;
-            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalPages = totalPages;
+
+            if (PageIndex > TotalPages)
+                PageIndex = TotalPages;
 
             this.AddRange(items);
         }
@@ -58,9 +61,14 @@ namespace BookStore
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
             var count = await source.CountAsync();
+            var totalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            if (pageIndex > totalPages)
+                pageIndex = totalPages;
+
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
 
-            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+            return new PaginatedList<T>(items, count, pageIndex, totalPages);
         }
     }
 }
