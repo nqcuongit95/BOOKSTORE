34ab6f1918ae04319fe649410c7b1065bc436313
Nguyen Quoc Cuong <nqcuongit95@gmail.com>
2016-12-18 15:25:27 +0700

    update database, them phieu nhan hang

--- src/BookStore/Models/Trangthai.cs
+++ src/BookStore/Models/Trangthai.cs
@@ -9,13 +9,16 @@ namespace BookStore.Models
         {
             DonHang = new HashSet<DonHang>();
             HangHoa = new HashSet<HangHoa>();
+            PhieuNhapHang = new HashSet<PhieuNhapHang>();
         }
 
         public int Id { get; set; }
         public string TenTrangThai { get; set; }
         public string VietTat { get; set; }
+        public string Loai { get; set; }
 
         public virtual ICollection<DonHang> DonHang { get; set; }
         public virtual ICollection<HangHoa> HangHoa { get; set; }
+        public virtual ICollection<PhieuNhapHang> PhieuNhapHang { get; set; }
     }
 }
