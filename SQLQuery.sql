42654bdd72d7186db5b726c622b72a2c53822029
Tran Anh Tuat <trananh200195@gmail.com>
2018-01-04 22:19:58 +0700

    delete old query

--- SQLQuery.sql
+++
@@ -1,8 +0,0 @@
-use BOOKSTORE;
-
-DBCC CHECKIDENT ('dbo.NhaCungCap',RESEED,0)
-DBCC CHECKIDENT ('dbo.NhanHieu',RESEED,0)
-DBCC CHECKIDENT ('dbo.TrangThai',RESEED,0)
-DBCC CHECKIDENT ('dbo.KhachHang',RESEED,0)
-
-
